cc44c0bac42cfae73482893da52a1975d29ac7bc
lsnq <pv@lsnq.ru>
2018-03-19 23:08:30 +0300

    Mocha: первый тест + скрипт в package.json

---
+++ test/test.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+describe('Init test', function() {
+  it('should return greeting', function() {
+    assert.equal('greeting', ['best','greeting','ever'][1]);
+  });
+});
