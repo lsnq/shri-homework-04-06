711708f36a152428005f3683bcf14090d123d1b1
lsnq <pv@lsnq.ru>
2018-03-21 15:53:43 +0300

    Тестовый коммит

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World 82!');
+    res.send('Hello Heroku!');
 });
 
 const port = process.env.PORT || process.env.$PORT || 8080;
