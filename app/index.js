f30697a69ecfb98c24b15f4100d2bdcc8452d5d8
lsnq <pv@lsnq.ru>
2018-03-21 17:38:13 +0300

    pr: commit 2

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Heroku!');
+    res.send('Hello Heroku Pull Request!');
 });
 
 const port = process.env.PORT || process.env.$PORT || 8080;
