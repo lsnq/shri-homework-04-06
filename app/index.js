8aee6802f03e59502a0e6064fa4ab9bb51534964
lsnq <pv@lsnq.ru>
2018-03-22 12:50:38 +0300

    fix port heroku

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join('dist/assets')));
 
 app.use('/', router);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
