d7f503a8101228750036899cce4bbb22c926b845
lsnq <pv@lsnq.ru>
2018-03-23 09:20:14 +0300

    test commit temp folder

--- app/git.js
+++ app/git.js
@@ -3,33 +3,37 @@ import {exec} from 'child_process';
 const execute = promisify(exec);
 
 const gitCli = async (script) => {
-    const {stdout} = await execute('git ' + script);
+    const {stdout, stderr} = await execute('git ' + script);
     return stdout;
 };
 
 const COMMANDS = {
-    getBranches: 'branch --format="%(refname:short)"',
-    getTree: 'ls-tree --abbrev'
+    branch: 'branch --format="%(refname:short)"',
+    tree: 'ls-tree --abbrev'
 };
 
 class GitData {
     async branches() {
-        const data = await gitCli(COMMANDS.getBranches);
+        const data = await gitCli(COMMANDS.branch);
         return data.split('\n').filter((el) => Boolean(el));
     }
     async tree(name) {
-        const treeRaw = await gitCli(COMMANDS.getTree + ' ' + name);
-        const treeArray = treeRaw.split('\n').filter((el) => Boolean(el));
-        return treeArray.map((el) => {
-            const chunks = el.split(/\s+/);
-            chunks.shift();
-            const item = {
-                type: chunks[0],
-                hash: chunks[1],
-                name: chunks[2]
-            };
-            return item;
-        });
+        try {
+            const treeRaw = await gitCli(COMMANDS.tree + ' ' + name);
+            const treeArray = treeRaw.split('\n').filter((el) => Boolean(el));
+            return treeArray.map((el) => {
+                const chunks = el.split(/\s+/);
+                chunks.shift();
+                const item = {
+                    type: chunks[0],
+                    hash: chunks[1],
+                    name: chunks[2]
+                };
+                return item;
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
