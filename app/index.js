1f7a5d7736f13bd8ca9acce8e9f78039e86da249
lsnq <pv@lsnq.ru>
2018-03-23 18:44:19 +0300

    lint

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 /*eslint no-console: off*/
-const router = require( './router');
-const path = require( 'path');
-const express = require( 'express');
+const router = require('./router');
+const path = require('path');
+const express = require('express');
 
 const app = express();
 
--- app/router.js
+++ app/router.js
@@ -39,5 +39,4 @@ router.get('/commit/:hash', async (req, res) => {
     });
 });
 
-
 module.exports = router;
